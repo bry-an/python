774cd6c9771c1d9401fa934b7f88829526d9d2f0
Bryan Yunis <bbyunis@gmail.com>
2019-04-06 11:17:11 -0600

    add turn generator

--- monster-battle/main.py
+++ monster-battle/main.py
@@ -3,29 +3,73 @@ from num2words import num2words
 
 
 def create_monster(pos):
+    # TODO: BETTER INPUT VALIDATION!!!
     # Name
     name = input(
         f"Enter a name for the {num2words(pos, to='ordinal')} monster: ")
     # Height
-    height = input(f"  How tall is {name} (1-10)? ")
+    height = int(input(f"  How tall is {name} (1-10)? "))
     # Weight
-    weight = input(f"  How much does {name} weigh (50-150)? ")
+    weight = int(input(f"  How much does {name} weigh (50-150)? "))
     # Damage
-    damage = input(f"  How hard does {name} hit (1-10)? ")
+    damage = int(input(f"  How hard does {name} hit (1-10)? "))
     # Defense
-    defense = input(f"  How much damage can {name} absorb at once (1-10)? ")
+    defense = int(
+        input(f"  How much damage can {name} absorb at once (1-10)? "))
+    this_monster = monster(name, height, weight, damage, defense)
+    return this_monster
 
-    new_monster = monster(name, height, weight, damage, defense)
 
-    return new_monster
+def turn_tracker(A, B):
+    '''Generates a sequence of turns, given two initiatives
+
+    Input: Two initiatives (i) such that 1 <= i <= 100
+    Output: Sequence of turns, 0==A, 1==B'''
+
+    # Initialize a counter and a "double" tracker
+    c, dbl = 1, False
+    while True:
+        if c % A == 0 and c % B == 0:
+            # Both monsters attack during this "clock tick"
+            yield int(dbl)
+            dbl = not dbl
+        elif c % A == 0:
+            # Only monster A attacks
+            yield 0
+        elif c % B == 0:
+            # Only monster B attacks
+            yield 1
+        if not dbl:
+            c = c % 100 + 1
+        # Note: This generator will frequently loop without yielding a result
 
 
-def battle(monster_one, monster_two):
-    pass
+def battle(*monst):
+
+    turn = turn_tracker(monst[0].initiative, monst[1].initiative)
+
+    # Get the width we need to display hit point updates
+    w = sum(len(str(m.hit_points)) for m in monst) + 4
+
+    while monst[0].alive() and monst[1].alive():
+        attacker = next(turn)
+        defender = not attacker
+        # Print the hit points update
+        print(f"{monst[0].hit_points} <> {monst[1].hit_points}".ljust(
+            w), end='\r', flush=True)
+        monst[defender].defend(monst[attacker].damage)
+
+    print('')
+    print(f"{monst[0].name if monst[0].alive() else monst[1].name} wins!")
 
 
 if __name__ == "__main__":
     print('Welcome to Monster Battle!')
-    monster_one = create_monster(1)
-    monster_two = create_monster(2)
+    # monster_one = create_monster(1)
+    # monster_two = create_monster(2)
+    monster_one = monster('a', 3, 50, 3, 3)
+    monster_two = monster('b', 10, 150, 10, 10)
+    print(monster_one)
+    print(monster_two)
+    battle(monster_one, monster_two)
     pass
--- monster-battle/monster.py
+++ monster-battle/monster.py
@@ -1,6 +1,9 @@
+from math import ceil
+
+
 class monster():
+
     def __init__(self, name, height, weight, damage, defense):
-        # Feed its own instance into itSELF
         # These are the user-selectable attributes
         self.name = name
         self.height = height
@@ -10,11 +13,30 @@ class monster():
 
         # We will compute the following attributes
         # Hit Points
+        # Random-ish formula.  Can tune this later as necessary.
+        self.hit_points = int((1.5 * self.height) *
+                              (.75 * self.weight) * (1.2 * self.defense) * 100)
         # Initiative
+        # This should be an integer in the 1-100 range, given our desired input ranges
+        self.initiative = ceil((self.height * self.weight) / 15)
         # Chance to stun
+        # This should be a float between .005 and .5
+        self.stun_chance = round(.05 * self.damage / self.defense, 5)
 
         # Current state of the monster
         # Normal/stunned
+        self.stunned = False
+
+    def __str__(self):
+        return f"""{self.name}
+      Height: {self.height}
+      Weight: {self.weight}
+      Damage: {self.damage}
+      Defense: {self.defense}
+      Initiative: {self.initiative}
+      Stun Chance: {self.stun_chance:.3%}
+      Current Status: {'*** Stunned ***' if self.stunned else 'Ready'}
+      Current Hit Points: {self.hit_points}"""
 
     def attack(self):
         pass
